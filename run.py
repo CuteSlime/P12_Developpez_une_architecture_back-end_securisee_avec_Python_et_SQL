c3e775ab0777d85c817001b216f5873776515cce
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-07-08 20:57:45 +0200

    succes connect to DB

--- run.py
+++ run.py
@@ -1,15 +1,24 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-from Models import User
+import os
 
-mysql_user = ""
-mysql_pwd = ""
-database = "epic_events"
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+db_user = os.environ.get('DB_USER')
+db_pwd = os.environ.get('DB_PWD')
+db_host = os.environ.get('DB_HOST', "localhost")
+db_port = os.environ.get('DB_PORT', "3306")
+db_name = os.environ.get('DB_NAME', "epic_events")
 
 
 engine = create_engine(
-    f'mysql://{mysql_user}:{mysql_pwd}@localhost/{database}')
+    f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}')
 
-with Session(engine) as session:
-    users = session.execute(select(User)).scalars().all()
+try:
+    connect = engine.connect()
+    print("success!")
+except Exception as ex:
+    print(ex)
+    # with Session(engine) as session:
+    #     users = session.execute(select(User)).scalars().all()
