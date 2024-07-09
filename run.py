1e344b8f5f234f9232d3b3b2a5b21c01f2e2229e
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-07-09 20:00:32 +0200

    refactor run.py

--- run.py
+++ run.py
@@ -1,24 +1,14 @@
-import os
-
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-
-load_dotenv()
-
-db_user = os.environ.get('DB_USER')
-db_pwd = os.environ.get('DB_PWD')
-db_host = os.environ.get('DB_HOST', "localhost")
-db_port = os.environ.get('DB_PORT', "3306")
-db_name = os.environ.get('DB_NAME', "epic_events")
+from Views.view import Views
+from Controllers.controller_menu import Menu
+from Controllers.controller import Controller
 
 
-engine = create_engine(
-    f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}')
+def main():
+    view = Views()
+    menu = Menu(view)
+    logiciel = Controller(view, menu)
+    logiciel.run()
 
-try:
-    connect = engine.connect()
-    print("success!")
-except Exception as ex:
-    print(ex)
-    # with Session(engine) as session:
-    #     users = session.execute(select(User)).scalars().all()
+
+if __name__ == "__main__":
+    main()
