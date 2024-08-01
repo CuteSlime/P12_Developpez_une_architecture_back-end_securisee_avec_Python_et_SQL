621a09fe1a9b8135c507e877bd05bd213e82ab48
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-08-01 15:38:44 +0200

    config JWT into user model

--- Models/model_user.py
+++ Models/model_user.py
@@ -1,9 +1,18 @@
+from datetime import datetime, timedelta, timezone
+import os
 from typing import List
-
+from dotenv import load_dotenv
+import jwt
 from argon2 import PasswordHasher
 from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .model_base import Base
+
+load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALGORITHM = os.environ.get('ALGORITHM', "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES'))
 
 
 class User(Base):
@@ -29,3 +38,23 @@ class User(Base):
     def check_password(self, password):
         ph = PasswordHasher()
         return ph.verify(self.password, password)
+
+    @staticmethod
+    def create_access_token(data: dict, expires_delta: timedelta = None):
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.now(timezone.utc) + expires_delta
+        else:
+            expire = datetime.now(timezone.utc)
+            expire += timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        return encoded_jwt
+
+    @staticmethod
+    def decode_access_token(token: str):
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            return payload
+        except jwt.PyJWTError:
+            return None
