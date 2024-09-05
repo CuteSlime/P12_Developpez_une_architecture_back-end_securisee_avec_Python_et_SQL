7ee3b5899d50b1e46ed53cdc58149f81af147ded
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-09-05 20:28:37 +0200

    update permission

--- permissions.py
+++ permissions.py
@@ -3,11 +3,14 @@ class PermissionManager:
         self.permissions = {
             "Management": {"Exit", "Validate_Change",
                            "user_menu", "handle_create_user", "handle_get_user", "handle_update_user", "handle_delete_user",
+                           "Update_user_fullname", "Update_user_email", "Update_user_password", "Update_user_group",
                            "group_menu",
                            "customer_menu", "handle_get_customer",
                            "contract_menu", "handle_create_contract", "handle_get_contract", "handle_update_contract",
                            # should be able to filter the event, update only to update the Support
-                           "event_menu", "handle_get_event", "handle_update_event", "Update_Support"},
+                           "event_menu", "handle_get_event", "handle_update_event",
+                           "Update_event_support"
+                           },
             "Commercial": {"Exit", "Validate_Change",
                            # update only customer that he is responsible for
                            "customer_menu", "handle_create_customer", "handle_get_customer", "handle_update_customer",
