e21abe5b17f7dee280c5d511f1d1efe3e44eea14
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-09-11 21:03:45 +0200

    update permission

--- permissions.py
+++ permissions.py
@@ -2,27 +2,38 @@ class PermissionManager:
     def __init__(self):
         self.permissions = {
             "Management": {"Exit", "Validate_Change",
-                           "user_menu", "handle_create_user", "handle_get_user", "handle_update_user", "handle_delete_user",
+                           "user_menu", "handle_create_user", "handle_get_user",
+                           "handle_update_user", "handle_delete_user",
                            "Update_user_fullname", "Update_user_email", "Update_user_password", "Update_user_group",
                            "group_menu",
                            "customer_menu", "handle_get_customer",
-                           "contract_menu", "handle_create_contract", "handle_get_contract", "handle_update_contract",
-                           # should be able to filter the event, update only to update the Support
+                           "contract_menu", "handle_create_contract",
+                           "handle_get_contract", "handle_update_contract",
+                           "Update_contract_customer", "Update_contract_total_price",
+                           "Update_contract_remaining_to_pay", "Update_contract_statut",
+                           # should be able to filter the event,
                            "event_menu", "handle_get_event", "handle_update_event",
                            "Update_event_support"
                            },
             "Commercial": {"Exit", "Validate_Change",
-                           # update only customer that he is responsible for
+
                            "customer_menu", "handle_create_customer", "handle_get_customer", "handle_update_customer",
-                           # update only contract that he is responsible for, filter on sign or remaining price
+                           "Update_customer_information", "Update_customer_fullname", "Update_customer_email",
+                           "Update_customer_phone_number", "Update_customer_company_name", "Update_customer_commercial",
+                           # filter on sign or remaining price
                            "contract_menu", "handle_get_contract", "handle_update_contract",
-                           # create event only for this own customer that have signed a contract
-                           "event_menu", "handle_create_event", "handle_get_event"},
+                           "Update_contract_customer", "Update_contract_total_price",
+                           "Update_contract_remaining_to_pay", "Update_contract_statut",
+                           "event_menu", "handle_create_event", "handle_get_event"
+                           },
             "Support": {"Exit", "Validate_Change",
                         "customer_menu", "handle_get_customer",
                         "contract_menu", "handle_get_contract",
-                        # filter event to see event who is attributed to them, update only event they are responsible of.
-                        "event_menu", "handle_get_event", "handle_update_event"}
+                        # filter event to see event who is attributed to them,
+                        "event_menu", "handle_get_event", "handle_update_event",
+                        "Update_event_contract", "Update_event_customer", "Update_event_start", "Update_event_end",
+                        "Update_event_support", "Update_event_location", "Update_event_atendees", "Update_event_notes",
+                        }
         }
 
     def has_permission(self, role_name, menu_name):
